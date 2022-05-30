c00b7a18b0055f2f7837755ad6b4b95a86a916ba
cnboker <coobots@gmail.com>
2022-05-30 16:33:10 +0800

    add juqery-weui

--- webhook-setup/deploy.sh
+++ webhook-setup/deploy.sh
@@ -5,22 +5,19 @@ git pull
 #删除未被使用的资源
 docker system prune -f
 #delete old container
-docker stop reverseproxy-authapi-1
-docker stop reverseproxy-fileapi-1
-docker stop reverseproxy-serviceapi-1
-#docker stop reverseproxy-ffmepapi-1
-docker stop reverseproxy-jsxbuild-1
+
 #rebuild
 cd /home/ioliz/member
 docker-compose build
-cd /home/ioliz//fileServer
+cd /home/ioliz/fileServer
 docker-compose build
-cd /home/ioliz//service
+cd /home/ioliz/service
 docker-compose build
-cd /home/ioliz//jsxBuildServer
+cd /home/ioliz/jsxBuildServer
 docker-compose build
 #cd /home/ioliz//ffmpegServer
 #docker-compose build
-cd /home/ioliz//reverseProxy
-docker stop reverseproxy-reverseproxy-1
+cd /home/ioliz/reverseProxy
+docker-compose down
+docker-compose build
 docker-compose up
