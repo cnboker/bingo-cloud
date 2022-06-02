d3cef141b3c86deeb87aaf82efdcec2b0e6c0fca
cnboker <coobots@gamil.com>
2022-06-02 18:09:59 +0800

    update

--- dotnetcoreServer/shared/Utils/WebDirecoty.cs
+++ dotnetcoreServer/shared/Utils/WebDirecoty.cs
@@ -36,7 +36,7 @@ namespace Ioliz.Shared.Utils
                 type = "video";
             }
 
-            return string.Format("{0}/{1}?size=512x512&type={2}&user={3}", this.domain, fileName, type, this.userName);
+            return string.Format("{0}/{1}?size=512x512&type={2}&user={3}", this.domain + "/" + this.userName, fileName, type, this.userName);
         }
     }
 
