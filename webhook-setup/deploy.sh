61f76a362395263470a61241d73c7d5168007cc4
cnboker <coobots@gamil.com>
2022-06-05 04:37:00 +0800

    switch docker-compose to command mode from  webhook-setup/deploy.sh

--- webhook-setup/deploy.sh
+++ webhook-setup/deploy.sh
@@ -1,26 +1,70 @@
 #!bin/bash
+
+# Although Compose works in all environments, it's more focused
+# development and testing. Using Compose on a production environment is not recommended at all.
+
 cd /home/ioliz
 git reset --hard
 git pull
+
 #删除未被使用的资源
 docker system prune -f
-#delete old container
-
-#rebuild
-cd /home/ioliz/dotnetcoreServer/member
-docker-compose build
-cd /home/ioliz/dotnetcoreServer/fileServer
-docker-compose build
-cd /home/ioliz/dotnetcoreServer/service
-docker-compose build
-cd /home/ioliz/jsxBuildServer
-docker-compose build
-cd /home/ioliz/ffmpegServer
-docker-compose build
-cd /home/ioliz/reverseProxy
 docker network create myNetwork
-docker-compose down
-docker-compose build
-docker-compose up -d 
+#rebuild
+cd ../dotnetcoreServer
+#docker-compose build
+docker container stop authapi
+docker container rm authapi
+docker image build --tag authapi:latest --file ./member/Dockerfile . # ../:docker context
+# docker container run \
+#     --rm \ #自动删除旧的container
+#     --detach \ #后台执行
+#     --restart always \ #host 启动自动重启
+#     --name authapi \ #名字
+#     --publish 7800:7800 \ #端口map
+#     --network myNetwork \ #bridge network
+#     authapi:latest
+docker run --detach --restart always --name authapi --publish 7800:7800 --network myNetwork authapi:latest
+
+#cd ../dotnetcoreServer/fileServer
+#docker-compose build
+docker container stop fileapi
+docker container rm fileapi
+docker image build --tag fileapi:latest --file ./fileServer/Dockerfile .
+docker run --detach --restart always --name fileapi --publish 5000:5000 --network myNetwork --volume /home/data/files:/release/wwwroot fileapi:latest
+
+#cd ../dotnetcoreServer/service
+#docker-compose build
+docker container stop serviceapi
+docker container rm serviceapi
+docker image create serviceapi:latest --file ./service/Dockerfile .
+docker run --detach --restart always --name serviceapi --publish 6001:6001 --network myNetwork serviceapi:latest
+
+cd ../../jsxBuildServer
+#docker-compose build
+docker container stop jsxbuild
+docker container rm jsxbuild
+docker image create jsxbuild:latest .
+docker run --detach --restart always --name jsxbuild --publish 8888:8888 --network myNetwork jsxbuild:latest
+
+
+cd ../ffmpegServer
+#docker-compose build
+docker container stop ffmpegapi
+docker container rm ffmpegapi
+docker image create ffmpegapi:latest .
+docker run --detach --restart always --name ffmpegapi --publish 9000:9000 --network myNetwork ffmpegapi:latest
+
+
+cd ../reverseProxy
+#docker network create myNetwork
+docker container stop nginxserver
+docker container rm nginxserver
+docker image create nginxserver:latest .
+docker run --detach --restart always --name nginxserver --publish 80:80 --network myNetwork --volume /home/ubuntu/dist/app: /www/app --volumne /home/ubuntu/dist/app: /www/app nginxserver:latest
+
+#docker-compose down
+#docker-compose build
+#docker-compose up -d 
 
 #fileServer 经常出现数据目录无法挂载的情况，出现这种问题，直接使用命令docker-compose up --force-recreate
--- webhook-setup/deploy_dev.sh
+++ webhook-setup/deploy_dev.sh
@@ -1,20 +1,62 @@
 #!bin/bash
+# Although Compose works in all environments, it's more focused
+# development and testing. Using Compose on a production environment is not recommended at all.
+
 #删除未被使用的资源
 docker system prune -f
-
-#rebuild
-cd ../dotnetcoreServer/member
-docker-compose build
-cd ../dotnetcoreServer/fileServer
-docker-compose build
-cd ../dotnetcoreServer/service
-docker-compose build
-cd ../jsxBuildServer
-docker-compose build
-#cd ../ffmpegServer
-#docker-compose build
 docker network create myNetwork
+#rebuild
+cd ../dotnetcoreServer
+#docker-compose build
+docker container stop authapi
+docker container rm authapi
+docker image build --tag authapi:latest --file ./member/Dockerfile . # ../:docker context
+# docker container run \
+#     --rm \ #自动删除旧的container
+#     --detach \ #后台执行
+#     --restart always \ #host 启动自动重启
+#     --name authapi \ #名字
+#     --publish 7800:7800 \ #端口map
+#     --network myNetwork \ #bridge network
+#     authapi:latest
+docker run --detach --restart always --name authapi --publish 7800:7800 --network myNetwork authapi:latest
+
+#cd ../dotnetcoreServer/fileServer
+#docker-compose build
+docker container stop fileapi
+docker container rm fileapi
+docker image build --tag fileapi:latest --file ./fileServer/Dockerfile .
+docker run --detach --restart always --name fileapi --publish 5000:5000 --network myNetwork --volume /home/data/files:/release/wwwroot fileapi:latest
+
+#cd ../dotnetcoreServer/service
+#docker-compose build
+docker container stop serviceapi
+docker container rm serviceapi
+docker image create serviceapi:latest --file ./service/Dockerfile .
+docker run --detach --restart always --name serviceapi --publish 6001:6001 --network myNetwork serviceapi:latest
+
+cd ../../jsxBuildServer
+#docker-compose build
+docker container stop jsxbuild
+docker container rm jsxbuild
+docker image create jsxbuild:latest .
+docker run --detach --restart always --name jsxbuild --publish 8888:8888 --network myNetwork jsxbuild:latest
+
+
+cd ../ffmpegServer
+#docker-compose build
+docker container stop ffmpegapi
+docker container rm ffmpegapi
+docker image create ffmpegapi:latest .
+docker run --detach --restart always --name ffmpegapi --publish 9000:9000 --network myNetwork ffmpegapi:latest
+
+
 cd ../reverseProxy
-docker-compose down
-docker-compose build
-docker-compose up -d
+#docker network create myNetwork
+docker container stop nginxserver
+docker container rm nginxserver
+docker image create nginxserver:latest .
+docker run --detach --restart always --name nginxserver --publish 80:80 --network myNetwork --volume /home/ubuntu/dist/app: /www/app --volumne /home/ubuntu/dist/app: /www/app nginxserver:latest
+
+
+#fileServer 经常出现数据目录无法挂载的情况，出现这种问题，直接使用命令docker-compose up --force-recreate
