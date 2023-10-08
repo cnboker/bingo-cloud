982efef67415f1cf4e45c4708f6d599b744a6a1e
cnboker <coobots@gmail.com>
2023-10-08 23:14:15 +0800

    fix deploy_dev execute error!

--- webhook-setup/deploy_dev.sh
+++ webhook-setup/deploy_dev.sh
@@ -1,5 +1,22 @@
 #!bin/bash
 
+docker_container_rm(){
+    # Define the name of the Docker container you want to check and remove if it exists
+    container_name="$1"
+
+    # Check if the container exists
+    if docker ps -a --format '{{.Names}}' | grep -q "$container_name"; then
+        # Container exists, so stop and remove it
+        docker stop "$container_name"
+        docker rm "$container_name"
+        echo "Container '$container_name' has been removed."
+    else
+        echo "Container '$container_name' does not exist."
+    fi
+}
+
+code_bash_path="/home/$1/src/ioliz"
+echo "code_bash_path '$code_bash_path'"
 # Define a variable with docker network name
 network_name="myNetwork"
 # Although Compose works in all environments, it's more focused
@@ -17,7 +34,7 @@ fi
 #docker-compose build
 docker_container_rm "authapi"
 #~/src 为远程服务器目录
-docker image build --tag authapi:latest --file ~/src/ioliz/dotnetcoreServer/member/Dockerfile . # ../:docker context
+docker image build --tag authapi:latest --file "$code_bash_path"/dotnetcoreServer/member/Dockerfile "$code_bash_path"/dotnetcoreServer # "$code_bash_path"/dotnetcoreServer:docker context
 # docker container run \
 #     --rm \ #自动删除旧的container
 #     --detach \ #后台执行
@@ -30,43 +47,29 @@ docker run --detach --restart always --name authapi --publish 7800:7800 --networ
 
 #docker-compose build
 docker_container_rm "fileapi"
-docker image build --tag fileapi:latest --file ~/src/ioliz/dotnetcoreServer/fileServer/Dockerfile .
+docker image build --tag fileapi:latest --file "$code_bash_path"/dotnetcoreServer/fileServer/Dockerfile "$code_bash_path"/dotnetcoreServer
 docker run --detach --restart always --name fileapi --publish 5000:5000 --network "$network_name" --volume ~/data/files:/release/wwwroot fileapi:latest
 
 #docker-compose build
 docker_container_rm "serviceapi"
-docker image build --tag serviceapi:latest --file ~/src/ioliz/dotnetcoreServer/service/Dockerfile .
+docker image build --tag serviceapi:latest --file "$code_bash_path"/dotnetcoreServer/service/Dockerfile "$code_bash_path"/dotnetcoreServer
 docker run --detach --restart always --name serviceapi --publish 6001:6001 --network "$network_name" serviceapi:latest
 
 
 #docker-compose build
 docker_container_rm "jsxbuild"
-docker image build --tag jsxbuild:latest --file ~/src/ioliz/jsxBuildServer/Dockerfile .
+docker image build --tag jsxbuild:latest --file "$code_bash_path"/jsxBuildServer/Dockerfile "$code_bash_path"
 docker run --detach --restart always --name  jsxbuild --publish 8888:8888 --network "$network_name" jsxbuild:latest
 
 
 #docker-compose build
 docker_container_rm "ffmpegapi"
-docker image build --tag ffmpegapi:latest --file ~/src/ioliz/ffmpegServer/Dockerfile .
+docker image build --tag ffmpegapi:latest --file "$code_bash_path"/ffmpegServer/Dockerfile "$code_bash_path"
 docker run --detach --restart always --name ffmpegapi --publish 9000:9000 --network "$network_name" ffmpegapi:latest
 
 #docker network create myNetwork
 docker_container_rm "nginxserver"
-docker image build --tag nginxserver:latest --file ~/src/ioliz/reverseProxy/Dockerfile .
+docker image build --tag nginxserver:latest --file "$code_bash_path"/reverseProxy/Dockerfile "$code_bash_path"
 docker run --detach --restart always --name nginxserver --publish 80:80 --network "$network_name" --volume ~/www/app:/app --volume /www/home:/www nginxserver:latest
 
 
-docker_container_rm(){
-    # Define the name of the Docker container you want to check and remove if it exists
-    container_name="$1"
-
-    # Check if the container exists
-    if docker ps -a --format '{{.Names}}' | grep -q "$container_name"; then
-        # Container exists, so stop and remove it
-        docker stop "$container_name"
-        docker rm "$container_name"
-        echo "Container '$container_name' has been removed."
-    else
-        echo "Container '$container_name' does not exist."
-    fi
-}
