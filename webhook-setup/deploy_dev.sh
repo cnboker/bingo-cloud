6f26aa7b4656dd17b8b7cad78e72cdf0c16aca17
cnboker <coobots@gmail.com>
2023-10-08 20:46:20 +0800

    update deply_dev.sh

--- webhook-setup/deploy_dev.sh
+++ webhook-setup/deploy_dev.sh
@@ -1,9 +1,12 @@
 #!bin/bash
+
+# Define a variable with docker network name
+network_name="myNetwork"
 # Although Compose works in all environments, it's more focused
 # development and testing. Using Compose on a production environment is not recommended at all.
 #删除未被使用的资源
 docker system prune -f
-network_name = "myNetwork"
+
 if docker network inspect "$network_name" > /dev/null 2>&1; then
     echo "network '$network_name' exists."
 else
@@ -23,36 +26,36 @@ docker image build --tag authapi:latest --file ~/src/ioliz/dotnetcoreServer/memb
 #     --publish 7800:7800 \ #端口map
 #     --network myNetwork \ #bridge network
 #     authapi:latest
-docker run --detach --restart always --name authapi --publish 7800:7800 --network myNetwork authapi:latest
+docker run --detach --restart always --name authapi --publish 7800:7800 --network "$network_name" authapi:latest
 
 #docker-compose build
 docker container stop fileapi
 docker container rm fileapi
 docker image build --tag fileapi:latest --file ~/src/ioliz/dotnetcoreServer/fileServer/Dockerfile .
-docker run --detach --restart always --name fileapi --publish 5000:5000 --network myNetwork --volume ~/data/files:/release/wwwroot fileapi:latest
+docker run --detach --restart always --name fileapi --publish 5000:5000 --network "$network_name" --volume ~/data/files:/release/wwwroot fileapi:latest
 
 #docker-compose build
 docker container stop serviceapi
 docker container rm serviceapi
 docker image build --tag serviceapi:latest --file ~/src/ioliz/dotnetcoreServer/service/Dockerfile .
-docker run --detach --restart always --name serviceapi --publish 6001:6001 --network myNetwork serviceapi:latest
+docker run --detach --restart always --name serviceapi --publish 6001:6001 --network "$network_name" serviceapi:latest
 
 
 #docker-compose build
 docker container stop jsxbuild
 docker container rm jsxbuild
 docker image build --tag jsxbuild:latest --file ~/src/ioliz/jsxBuildServer/Dockerfile .
-docker run --detach --restart always --name  jsxbuild --publish 8888:8888 --network myNetwork jsxbuild:latest
+docker run --detach --restart always --name  jsxbuild --publish 8888:8888 --network "$network_name" jsxbuild:latest
 
 
 #docker-compose build
 docker container stop ffmpegapi
 docker container rm ffmpegapi
 docker image build --tag ffmpegapi:latest --file ~/src/ioliz/ffmpegServer/Dockerfile .
-docker run --detach --restart always --name ffmpegapi --publish 9000:9000 --network myNetwork ffmpegapi:latest
+docker run --detach --restart always --name ffmpegapi --publish 9000:9000 --network "$network_name" ffmpegapi:latest
 
 #docker network create myNetwork
 docker container stop nginxserver
 docker container rm nginxserver
 docker image build --tag nginxserver:latest --file ~/src/ioliz/reverseProxy/Dockerfile .
-docker run --detach --restart always --name nginxserver --publish 80:80 --network myNetwork --volume ~/www/app:/app --volume /www/home:/www nginxserver:latest
+docker run --detach --restart always --name nginxserver --publish 80:80 --network "$network_name" --volume ~/www/app:/app --volume /www/home:/www nginxserver:latest
